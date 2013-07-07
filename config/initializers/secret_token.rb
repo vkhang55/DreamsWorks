b3d62a6eb3102e6ec85b226e18095c39aae7fc1b
Khang Pham <vkhang55@gmail.com>
2013-07-06 20:19:02 -0700

    first DreamsWorks commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DreamsWorks::Application.config.secret_token = '85e90be9595bfa9d204a40e750cd8e7240bb0d3e1e8ff59453ac506a195c2f37cc766211a1bf5efee037d9c1dcba35898ab20b175c501854ec5249bac1e0a24a'
